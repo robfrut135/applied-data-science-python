4520cfb3a4a9f6c0992b9922e0efed841b17b12c
dxc-cp-tools <dxc-cp-automation@dxc.com>
2018-07-09 00:46:15 +0200

    (dev) doing practice 3

--- 02-Applied Plotting, Charting And Data Representation/Practice+3.py
+++ 02-Applied Plotting, Charting And Data Representation/Practice+3.py
@@ -1,28 +1,8 @@
-
-# coding: utf-8
-
-# # Practice Assignment: Understanding Distributions Through Sampling
-# 
-# ** *This assignment is optional, and I encourage you to share your solutions with me and your peers in the discussion forums!* **
-# 
-# 
-# To complete this assignment, create a code cell that:
-# * Creates a number of subplots using the `pyplot subplots` or `matplotlib gridspec` functionality.
-# * Creates an animation, pulling between 100 and 1000 samples from each of the random variables (`x1`, `x2`, `x3`, `x4`) for each plot and plotting this as we did in the lecture on animation.
-# * **Bonus:** Go above and beyond and "wow" your classmates (and me!) by looking into matplotlib widgets and adding a widget which allows for parameterization of the distributions behind the sampling animations.
-# 
-# 
-# Tips:
-# * Before you start, think about the different ways you can create this visualization to be as interesting and effective as possible.
-# * Take a look at the histograms below to get an idea of what the random variables look like, as well as their positioning with respect to one another. This is just a guide, so be creative in how you lay things out!
-# * Try to keep the length of your animation reasonable (roughly between 10 and 30 seconds).
-
-# In[ ]:
-
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.animation as animation
 
-get_ipython().magic('matplotlib notebook')
+%matplotlib notebook
 
 # generate 4 random variables from the random, gamma, exponential, and uniform distributions
 x1 = np.random.normal(-2.5, 1, 10000)
@@ -44,7 +24,37 @@ plt.text(x3.mean()-1.5, 0.5, 'x3\nExponential')
 plt.text(x4.mean()-1.5, 0.5, 'x4\nUniform')
 
 
-# In[ ]:
+##############################################################
 
+n = 1000
+x1 = np.random.normal(-2.5, 1, n)
+x2 = np.random.gamma(2, 1.5, n)
+x3 = np.random.exponential(2, n)
+x4 = np.random.uniform(14,20, n)
+
+axis_normal = [-6,1,0,50]
+axis_gamma = [-1,15,0,70]
+axis_exp = [-1,15,0,70]
+axis_unif = [13,21,0,20]
+axis_dist =[axis_normal, axis_gamma, axis_exp, axis_unif]
+
+xs = [x1,x2,x3,x4]
+
+def update(curr):
+    if curr == n:
+        a.event_source.stop()
+    bins_normal = np.arange(-6, 2, 0.8)
+    bins_gamma = np.arange(0, 30, 3)
+    bins_exp = np.arange(0, 10, 0.5)
+    bins_unif = np.arange(14, 20, 0.6)
+    bin_sample = [bins_normal, bins_gamma, bins_exp, bins_unif]
+    for i in range(0, len(axs)):
+        axs[i].cla()
+        axs[i].hist(xs[i][:curr], bins = bin_sample[i])
+        axs[i].axis(axis_dist[i])
+
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+axs = [ax1,ax2,ax3,ax4]
+a = animation.FuncAnimation(fig, update, interval=5)
 
 
